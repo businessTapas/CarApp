0d20dbacba520691fbec4700878cbeae417afee3
buisnessTapas <saha.28tapas@gmail.com>
2025-07-12 06:51:19 +0530

    multer-upload-folder

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,6 +1,12 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
+const uploadDir = path.join(__dirname, '../uploads');
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true }); // creates nested directories if needed
+}
 // Define storage engine
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
